d3fb105c1a8ffca9911118469f5a4703ec05dcbf
bekir <bkrylmz981@gmail.com>
2022-12-19 22:04:26 +0300

    bekir task 3

---
+++ src/mondayTrial/Task3.java
@@ -0,0 +1,10 @@
+package mondayTrial;
+
+public class Task3 {
+    public static void main(String[] args) {
+
+        int number= 11;
+        String str= "Hello";
+        System.out.println(str);
+    }
+}
